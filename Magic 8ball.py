27f873bb1bc9ad600248314bc1a506149aa79f2f
Daniel Archevald <124039519+KujoKay@users.noreply.github.com>
2024-05-24 01:21:20 -0400

    Add files via upload

---
+++ Magic 8ball.py
@@ -0,0 +1,41 @@
+import random
+
+name = "David"
+question = "Will I win the lottery?"
+answer = ""
+
+random_number = random.randint(1, 10)
+
+
+if random_number == 1:
+  answer = "Yes - definitely"
+elif random_number == 2:
+  answer = "It is decidely so"
+elif random_number == 3:
+  answer = "It is very likely"
+elif random_number == 4:
+  answer = "Without a doubt"
+elif random_number == 5:
+  answer = "Reply hazy, try again"
+elif random_number == 6:
+  answer = "Ask again later"
+elif random_number == 7:
+  answer = "Better not tell you now"
+elif random_number == 8:
+  answer = "My sources say no"
+elif random_number == 9:
+  answer = "Outlook not so good"
+elif random_number == 10:
+  answer = "Very doubtful"
+else:
+  answer = "Error"
+
+if name == "":
+  print("Question: " + question)
+else:
+ print(name + " asks: " + question)
+
+if question == "":
+  print("You must ask a question!")
+else:
+ print("Magic 8-ball's answer: " + answer )
